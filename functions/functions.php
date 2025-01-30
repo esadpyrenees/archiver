ceca6eca4ea738e565056c01ceca875a1004b734
erwansio1 <erwan.demay64140@gmail.com>
2025-01-30 16:02:06 +0100

    rebasing project from the main branch and adding style and display

--- functions/functions.php
+++ functions/functions.php
@@ -1,51 +1,56 @@
 <?php
 
-function hasIndex($dir){
-  $archivesdir = dirname(__FILE__) . "./root";    
+function hasIndex($dir)
+{
+  $archivesdir = dirname(__FILE__) . "./root";
   $archivesdir = $archivesdir . "/" . $dir;
-  $s = scandir( $dir);
-  $fs = array_diff( $s, array(".", "..") );
-  foreach($fs as $f){
-    if(in_array($f, ["index.html", "index.php", "index.htm"])){        
+  $s = scandir($dir);
+  $fs = array_diff($s, array(".", ".."));
+  foreach ($fs as $f) {
+    if (in_array($f, ["index.html", "index.php", "index.htm"])) {
       return $f;
-    } 
+    }
   }
   return false;
 }
 
 // Does the dir has an index.md file 
-  function hasMDIndex($dir){
-    foreach(glob($dir.'/index.md',GLOB_BRACE) as $file){
-      return $file;
-    }
-    return false;
-  }
-
-    function isEmpty($dir){
-    return !(new \FilesystemIterator($dir))->valid();
-  }
-   
-    
-
-
-  function hasSubDirectories($dir){
-    foreach (new DirectoryIterator($dir) as $fileinfo) {
-      if ($fileinfo->isDir() && !$fileinfo->isDot()) {
-        return true;
-      }
-    }return false;
-  }
-  
-
-  function calculateFolderSize ($dir)
+function hasMDIndex($dir)
 {
-    $size = 0;
+  foreach (glob($dir . '/index.md', GLOB_BRACE) as $file) {
+    return $file;
+  }
+  return false;
+}
 
-    foreach (glob(rtrim($dir, '/').'/*', GLOB_NOSORT) as $each) {
-        $size += is_file($each) ? filesize($each) : calculateFolderSize($each);
+function isEmpty($dir)
+{
+  return !(new \FilesystemIterator($dir))->valid();
+}
+
+
+
+
+function hasSubDirectories($dir)
+{
+  foreach (new DirectoryIterator($dir) as $fileinfo) {
+    if ($fileinfo->isDir() && !$fileinfo->isDot()) {
+      return true;
     }
+  }
+  return false;
+}
 
-    return $size;
+
+function calculateFolderSize($dir)
+{
+  $size = 0;
+
+  foreach (glob(rtrim($dir, '/') . '/*', GLOB_NOSORT) as $each) {
+    $size += is_file($each) ? filesize($each) : calculateFolderSize($each);
+  }
+
+  return $size;
 }
 
 
@@ -54,37 +59,39 @@ function hasIndex($dir){
 
 //convert bytes 
 function sizeFilter($bytes)
-    {
-        $label = array('B', 'KB', 'MB', 'GB', 'TB', 'PB');
+{
+  $label = array('B', 'KB', 'MB', 'GB', 'TB', 'PB');
 
-        for ($i = 0; $bytes >= 1024 && $i < (count($label) - 1); $bytes /= 1024, $i++) ;
+  for ($i = 0; $bytes >= 1024 && $i < (count($label) - 1); $bytes /= 1024, $i++);
 
-        return (round($bytes, 2) . " " . $label[$i]);
-    }
+  return (round($bytes, 2) . " " . $label[$i]);
+}
 
 
-    //trying this function to cache the folders size to optimise application
-  function getCachedFolderSize($dir){
-    $cacheFile = sys_get_temp_dir() . "folder_size_cache_" . md5($dir)	. '.txt' ;
-    if (file_exists($cacheFile) && (time() - filemtime($cacheFile) < 3600)){
-      return file_get_contents($cacheFile);
-    } else {
-      $size = calculateFolderSize($dir);
-      file_put_contents($cacheFile, $size);
-      return $size;
-    }
+//trying this function to cache the folders size to optimise application
+function getCachedFolderSize($dir)
+{
+  $cacheFile = sys_get_temp_dir() . "folder_size_cache_" . md5($dir)  . '.txt';
+  if (file_exists($cacheFile) && (time() - filemtime($cacheFile) < 3600)) {
+    return file_get_contents($cacheFile);
+  } else {
+    $size = calculateFolderSize($dir);
+    file_put_contents($cacheFile, $size);
+    return $size;
   }
+}
 
-  //function to clear cache
-  function clearCache($dir){
-    $cacheFile = sys_get_temp_dir() . "/folder_size_cache_" .md5($dir) . '.txt';
-    if (file_exists($cacheFile)){
-      unlink($cacheFile);
-    }
+//function to clear cache
+function clearCache($dir)
+{
+  $cacheFile = sys_get_temp_dir() . "/folder_size_cache_" . md5($dir) . '.txt';
+  if (file_exists($cacheFile)) {
+    unlink($cacheFile);
   }
+}
 
 
-  /*fonctions pour pouvoir tester la performance de la mise en cache et de l'affichage des tailles des dossiers
+/*fonctions pour pouvoir tester la performance de la mise en cache et de l'affichage des tailles des dossiers
 
   function startTimer() {
   return microTime(true);
@@ -96,7 +103,3 @@ function sizeFilter($bytes)
   
 
   */
-
-
-
-  ?>
--- index.php
+++ index.php
@@ -1,16 +1,16 @@
-<?php 
+<?php
 include_once 'functions/functions.php';
 include_once '_inc/Parsedown.php';
 include_once '_inc/ParsedownExtra.php';
 $params = '';
 if (isset($_GET['params'])) {
-  $params = '/' . $_GET['params'];
+    $params = '/' . $_GET['params'];
 }
 
 // $archivesdir = '../../archives';
 
 $archivesdir = dirname(__FILE__) . "/archives";
-$currentdir = $archivesdir . $params; 
+$currentdir = $archivesdir . $params;
 
 // markdown!
 $Parsedown = new ParsedownExtra();
@@ -19,81 +19,112 @@ $Parsedown = new ParsedownExtra();
 //$startCached = startTimer();
 
 $results = array();
-$cool_extensions = Array('jpg','png','pdf','gif','webp','html','zip', 'css', 'js');
+$cool_extensions = array('jpg', 'png', 'pdf', 'gif', 'webp', 'html', 'zip', 'css', 'js');
+$forbidden_extensions = array('psd', 'tif', 'tiff', 'ai', 'indd');
 
 // browse currentdir, looking for subdirs or index*
-  if(is_dir($currentdir)){
-foreach (new DirectoryIterator($currentdir) as $fileinfo) {
-  if ($fileinfo->isDot()) continue; // Ignore . et ..
-                
-  if ($fileinfo->isDir()) {// Subfolder find 
-    $folderPath = $fileinfo->getPathname();
-    $results[] = [
-        'path' => $fileinfo->getFilename() . '/',
-        'name' => $fileinfo->getFilename() . '/',
-        'is_empty' => isEmpty($folderPath),
-        'size' => sizeFilter(getCachedFolderSize($folderPath))
-    ];
-  
-  } elseif (in_array($fileinfo->getExtension(), $cool_extensions)) {
-    $results[] = [
-        'path' => $fileinfo->getFilename(),
-        'name' => $fileinfo->getFilename(),
-        'is_empty' => false,
-        'size' => sizeFilter(filesize($fileinfo->getPathname())) // Taille des fichiers
-    ];
-  }
-}
-} elseif (is_file($currentdir)){
-  if(pathinfo($currentdir, PATHINFO_EXTENSION) === 'html') {
-    header("Location" . $_GET['params']);
-    exit;
-  } else{
-    echo " Error : THe specified path is not a directory or a valid HTML File.";
-    exit;
-  }
+if (is_dir($currentdir)) {
+    foreach (new DirectoryIterator($currentdir) as $fileinfo) {
+        if ($fileinfo->isDot()) continue; // Ignore . et ..
+
+        if ($fileinfo->isDir()) { // Subfolder find
+            $folderPath = $fileinfo->getPathname();
+            $has_forbidden = false;
+
+            // Check if the folder contains forbidden files / add true do the boolean $has_forbidden if it does 
+            foreach (new DirectoryIterator($folderPath) as $subfileinfo) {
+                if ($subfileinfo->isDot()) continue;
+                elseif (in_array($subfileinfo->getExtension(), $forbidden_extensions)) {
+                    $has_forbidden = true;
+                    break;
+                }
+            }
+
+            $results[] = [
+                'path' =>  $fileinfo->getFilename() . '/',
+                'name' => $fileinfo->getFilename() . '/',
+                'is_empty' => isEmpty($folderPath),
+                'size' => sizeFilter(getCachedFolderSize($folderPath)),
+                'has_forbidden' => $has_forbidden
+            ];
+        } elseif (in_array($fileinfo->getExtension(), $cool_extensions)) {
+            $results[] = [
+                'path' => $fileinfo->getFilename(),
+                'name' => $fileinfo->getFilename(),
+                'is_empty' => false,
+                'size' => sizeFilter(filesize($fileinfo->getPathname())), // Taille des fichiers
+                'has_forbidden' => false
+            ];
+        }
+    }
 }
 
-?> 
+define('WARNING_GLYPH', '⚠');
+define('EMPTY_GLYPH', '●');
 
-<main class="pane active" id="content">
-  <link rel="stylesheet" href="<?= str_replace("index.php", "style/style.css", $_SERVER['SCRIPT_NAME']) ?>">
-  <h1>Archives</h1> 
-  <nav class="archives-nav">
-    <!-- L’archivisme est un exercice délicat ☺<br><br> -->
-    <p>☺</p>
-    
-    <?php
-    
-      echo "<ul class='parentFolder'>";
-      if ($params) {
-        $up = dirname($currentdir);
-        $upname = basename($up);
-        $currentFolderName = basename($currentdir);	
-        echo "<li><a href='../'/>← $upname / $currentFolderName</a></li>";
-      }
-      // Sort and display the directory content
-      rsort($results);
-      echo "</ul>
+//TODO :au niveau des warnings mettre une erreur lors de l'ouverture du dossier contenant un fichier "interdit" ? 
+//TODO : Finir de corriger les erreurs validator (8 errors 1 warning actuellement)
+//TODO : ouvrir un dossier uqi contient un fichier .html (ouvrir directement le fichier)
+//TODO : verifier la meilleure solution pour la taille des dossiers/fichiers
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="" width=device-width, initial-scale="1.0" />
+    <title>Archives ESAD</title>
+    <link rel="stylesheet" href="<?= str_replace("index.php", "style/style.css", $_SERVER['SCRIPT_NAME']) ?>">
+</head>
+
+<body>
+    <main class="pane active" id="content">
+        <h1>Archives</h1>
+        <nav class="archives-nav">
+            <p>☺</p>
+            <?php
+            echo "<ul class='parentFolder'>";
+            if ($params) {
+                $up = dirname($currentdir);
+                $upname = basename($up);
+                $currentFolderName = basename($currentdir);
+                echo "<li><a href='../'/>← $upname / $currentFolderName</a></li>";
+            }
+
+            // Sort and display the directory content
+            rsort($results);
+            echo "</ul>
 
       <div class='displayFolders'>
       <ul style='list-style:none'>";
-      foreach ($results as $dir) {
+            foreach ($results as $dir) {
+                $glyphs = '';
+                if ($dir['has_forbidden']) {
+                    $glyphs .= WARNING_GLYPH . ' ';
+                }
+                if ($dir['is_empty']) {
+                    $glyphs .= EMPTY_GLYPH . ' ';
+                }
+                echo "<li class='file-info'>
+                 <span class='glyphs'>{$glyphs}</span>
+                    <a href='{$dir['path']}'>{$dir['name']}</a>
+                    <p>({$dir['size']})</p>
+                </li>";
+            }
+            echo "</ul>";
+            echo "</div>";
 
-          {
-            echo "<li class='file-info'><a href='" . $dir['path'] . "'>" . $dir['name'] . "</a> <p>(" . $dir['size'] . ")</p></li>";
-          }
-        }
-      
-      echo "</ul>
-      </div>";
+            // Display the content of index.md if it exists
+            $mdindex = hasMDIndex($currentdir);
+            if ($mdindex) {
+                echo "<hr>";
+                echo $Parsedown->text(file_get_contents($mdindex));
+            }
+            ?>
+        </nav>
+    </main>
+</body>
 
-      // Display the content of index.md if it exists
-      $mdindex = hasMDIndex($currentdir);
-      if ($mdindex) {
-          echo "<hr>";
-          echo $Parsedown->text(file_get_contents($mdindex));
-      }
-    ?>
-  </nav>
-</main>
+</html>
